36f91cf8cc9f6b8d0761db9bd0fbc18774e60093
arfat132 <arfatbegum90@gmail.com>
2022-05-23 23:12:07 +0300

    mongodb added

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 require('dotenv').config()
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://dbUser:<password>@cluster0.pwa7ybe.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('db conected')
+  client.close();
+});
 
 app.get('/', (req, res) => {
   res.send('Screwbeat is running')
