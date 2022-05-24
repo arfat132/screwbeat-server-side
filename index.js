d83ccff30b33e9e6eb439df38b4f49439942a72b
arfat132 <arfatbegum90@gmail.com>
2022-05-24 12:51:55 +0300

    tools reviews and orders api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 //middleware
@@ -17,8 +17,37 @@ async function run() {
     try {
         await client.connect();
         const toolsCollection = client.db("screwBeat").collection("tools");
-        console.log('db conected')
-        
+        const reviewsCollection = client.db("screwBeat").collection("reviews");
+        const ordersCollection = client.db("screwBeat").collection("orders");
+
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = toolsCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        });
+
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tool = await toolsCollection .findOne(query);
+            res.send(tool);
+        })
+
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const cursor = reviewsCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        app.post('/orders', async (req, res) => {
+            const newOrders = req.body;
+            const result = await ordersCollection.insertOne(newOrders);
+            res.send(result);
+
+        });
+       
     } finally {
 
     }
@@ -27,9 +56,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Screwbeat is running')
+    res.send('Screwbeat is running')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
