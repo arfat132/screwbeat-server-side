e9c2970149c42ab600ea83ddf903d33e29276009
arfat132 <arfatbegum90@gmail.com>
2022-05-24 09:13:25 +0300

    middleware added

--- index.js
+++ index.js
@@ -2,21 +2,29 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://dbUser:<password>@cluster0.pwa7ybe.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pwa7ybe.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('db conected')
-  client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const toolsCollection = client.db("screwBeat").collection("tools");
+        console.log('db conected')
+        
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Screwbeat is running')
